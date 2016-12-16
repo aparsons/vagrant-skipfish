772bcc46cfe43ecc85c8edb3f796fb0e6bf0b4f7
Adam Parsons <adam@aparsons.net>
2016-12-15 19:29:18 -0600

    finalizing

--- provision.sh
+++ provision.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get install -y build-essential libpcre3-dev libidn11-dev libssl-dev
 
 cd /vagrant/skipfish-2.10b
 make
+
+ln -s /vagrant/skipfish-2.10b/ ~/skipfish
